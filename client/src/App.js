c5d463c463370c418b2b1005f785b2ac1ad7c7ca
patrickwide <patrickwide254@gmail.com>
2023-06-25 12:36:51 +0300

    Complete lab

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+
+function App() {
+  useEffect(() => {
+    fetch("/movies")
+      .then((res) => res.json())
+      .then((movies) => console.log(movies));
+  }, []);
+
+  return <h1>Hello from React!</h1>;
+}
+
+export default App;
